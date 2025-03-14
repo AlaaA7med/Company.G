e98eb06e0c0bf641b306f2fbe25995acdb657e2f
lenovo <lenovo@DESKTOP-QBP9RUO>
2025-03-15 00:20:57 +0200

    Department Module BLL

---
+++ Company.G.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G.DAL.Models;
+
+namespace Company.G.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int id);
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
