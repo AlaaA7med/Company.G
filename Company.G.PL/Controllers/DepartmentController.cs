9e03ebe47344043ca361f8499703290d1efdfde4
lenovo <lenovo@DESKTOP-QBP9RUO>
2025-03-15 22:29:10 +0200

    Create Create(Action+View)

--- Company.G.PL/Controllers/DepartmentController.cs
+++ Company.G.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G.BLL.Interfaces;
 using Company.G.BLL.Repositories;
+using Company.G.DAL.Models;
+using Company.G.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G.PL.Controllers
@@ -17,6 +19,30 @@ namespace Company.G.PL.Controllers
         {
             var departments= _departmentRepository.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = new Department()
+                {
+                    Code=model.Code, 
+                    Name=model.Name, 
+                    CreateAt=model.CreateAt
+                };
+                var count=_departmentRepository.Add(department);
+                if (count > 0) 
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
         }
     }
 }
