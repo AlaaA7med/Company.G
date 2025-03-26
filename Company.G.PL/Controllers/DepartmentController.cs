fd02449a35470a8fa3d7847fb50f6b5d806e9a8e
lenovo <lenovo@DESKTOP-QBP9RUO>
2025-03-26 23:08:22 +0200

    Create Delete Action

--- Company.G.PL/Controllers/DepartmentController.cs
+++ Company.G.PL/Controllers/DepartmentController.cs
@@ -117,5 +117,38 @@ namespace Company.G.PL.Controllers
         //    }
         //    return View(model);
         //}
+
+
+        //------------------
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id ");
+
+            var department = _departmentRepository.Get(id.Value);
+
+            if (department is null) return NotFound(new { StatusCode = 404, Message = $"Department With Id : {id} is not found" });
+
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != department.Id) return BadRequest();
+
+                var count = _departmentRepository.Delete(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+
     }
 }
